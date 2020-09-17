424ef5c302c75917a25102eef062120b6846d824
bant <bant62@gmail.com>
2020-09-17 09:08:32 +0900

    生徒情報修正

--- resources/views/admin/user/info.blade.php
+++ resources/views/admin/user/info.blade.php
@@ -3,42 +3,44 @@
 @section('content')
 <div id="content">
     <section>
-    <h3>生徒の詳細</h3>
-    <div class="row justify-content-center">
-        <div class="col-md-10">
-            <div class="card">
-                <div class="card-header"><i class="fas fa-align-justify"></i> 生徒の詳細</div>
-                <div class="card-body">
-                    {{-- ユーザー1件の情報 --}}
-                    <dl class="row">
-                        <dt class="col-md-2">{{ __('ID') }}</dt>
-                            <dd class="col-md-10">{{ $user->id }}</dd>
-                        <dt class="col-md-2">{{ __('Name') }}</dt>
+        <h3>生徒の詳細</h3>
+        <div class="row justify-content-center">
+            <div class="col-md-10">
+                <div class="card">
+                    <div class="card-header"><i class="fas fa-align-justify"></i> 生徒の詳細</div>
+                    <div class="card-body">
+                        {{-- ユーザー1件の情報 --}}
+                        <dl class="row">
+                            <dt class="col-md-2">お名前</dt>
                             <dd class="col-md-10">{{ $user->name }}</dd>
-                        <dt class="col-md-2">{{ __('E-Mail Address') }}</dt>
+                            <dt class="col-md-2">読みがな</dt>
+                            <dd class="col-md-10">{{ $user->kana }}</dd>
+                            <dt class="col-md-2">生徒ID</dt>
+                            <dd class="col-md-10">{{ $user->id }}</dd>
+                            <dt class="col-md-2">メールアドレス</dt>
                             <dd class="col-md-10">{{ $user->email }}</dd>
-                        <dt class="col-md-2">郵便番号</dt>
+                            <dt class="col-md-2">郵便番号</dt>
                             <dd class="col-md-10">{{ $user->zip_code }}</dd>
-                        <dt class="col-md-2">住所</dt>
+                            <dt class="col-md-2">住所</dt>
                             <dd class="col-md-10">{{ $user->pref }}{{ $user->address }}</dd>
-                        <dt class="col-md-2">電話</dt>
+                            <dt class="col-md-2">電話</dt>
                             <dd class="col-md-10">{{ $user->tel }}</dd>
-                        <dt class="col-md-2">誕生日</dt>
+                            <dt class="col-md-2">誕生日</dt>
                             <dd class="col-md-10">{{ $user->birthday }}</dd>
-                        <dt class="col-md-2">性別</dt>
-                        @if ($user->gender=='male')
+                            <dt class="col-md-2">性別</dt>
+                            @if ($user->gender=='male')
                             <dd class="col-md-10">男</dd>
-                        @else
+                            @else
                             <dd class="col-md-10">女</dd>
-                        @endif
-                        <dt class="col-md-2">ポイント</dt>
+                            @endif
+                            <dt class="col-md-2">保有ポイント</dt>
                             <dd class="col-md-10">{{number_format($user->point)}}pt</dd>
-                    </dl>
-                    <a class="float-center btn btn-sm btn-primary" href="#" onclick="javascript:window.history.back(-1);return false;">戻る</a>
+                        </dl>
+                        <a class="float-center btn btn-sm btn-primary" href="#" onclick="javascript:window.history.back(-1);return false;">戻る</a>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
 </div>
 @endsection
--- resources/views/staff/user/info.blade.php
+++ resources/views/staff/user/info.blade.php
@@ -2,56 +2,69 @@
 
 @section('content')
 <div id="content">
-<section>
-    <h3>生徒の詳細</h3>
-    <div class="row justify-content-center">
-        <div class="col-md-10">
-            <div class="card">
-                <div class="card-header"><i class="fas fa-align-justify"></i>{{$user->name}}さんの詳細</div>
+    <section>
+        <h3>生徒の詳細</h3>
+        <div class="row justify-content-center">
+            <div class="col-md-10">
+                <div class="card">
+                    <div class="card-header"><i class="fas fa-align-justify"></i>{{$user->name}}さんの詳細</div>
 
-                <div class="card-body">
-                    @if (session('status'))
+                    <div class="card-body">
+                        @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
-                    @endif
+                        @endif
 
-                    {{--成功時のメッセージ--}}
-                    @if (session('success'))
-                      <div class="alert alert-success">{{ session('success') }}</div>
-                    @endif
-                    {{-- エラーメッセージ --}}
-                    @if ($errors->any())
+                        {{--成功時のメッセージ--}}
+                        @if (session('success'))
+                        <div class="alert alert-success">{{ session('success') }}</div>
+                        @endif
+                        {{-- エラーメッセージ --}}
+                        @if ($errors->any())
                         <div class="alert alert-danger">
-                        <ul>
-                            @foreach ($errors->all() as $error)
+                            <ul>
+                                @foreach ($errors->all() as $error)
                                 <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
+                                @endforeach
+                            </ul>
                         </div>
-                    @endif
+                        @endif
 
-                    {{-- 生徒の情報 --}}
-                    <dl class="row">
-                        <dt class="col-md-4">お名前名</dt>
-                        <dd class="col-md-8">{{ $user->name }}</dd>
-                        <dt class="col-md-4">生徒ID</dt>
-                        <dd class="col-md-8">{{ $user->id }}</dd>
-                        <dt class="col-md-4">メールアドレス</dt>
-                        <dd class="col-md-8">{{ $user->email }}</dd>
-                        <dt class="col-md-4">住所</dt>
-                        <dd class="col-md-8">{{ $user->address }}</dd>
-                        <dt class="col-md-4">教室参加回数</dt>
-                        <dd class="col-md-8">{{ $class_reservation_times }}回</dd>
-                        <dt class="col-md-4">オンライン教室参加回数</dt>
-                        <dd class="col-md-8">{{ $zoom_reservation_times }}回</dd>
-                   </dl>
+                        {{-- 生徒の情報 --}}
+                        <dl class="row">
+                            <dt class="col-md-4">お名前</dt>
+                            <dd class="col-md-8">{{ $user->name }}</dd>
+                            <dt class="col-md-4">読みがな</dt>
+                            <dd class="col-md-8">{{ $user->kana }}</dd>
+                            <dt class="col-md-4">生徒ID</dt>
+                            <dd class="col-md-8">{{ $user->id }}</dd>
+                            <dt class="col-md-4">メールアドレス</dt>
+                            <dd class="col-md-8">{{ $user->email }}</dd>
+                            <dt class="col-md-4">郵便番号</dt>
+                            <dd class="col-md-8">{{ $user->zip_code }}</dd>
+                            <dt class="col-md-4">住所</dt>
+                            <dd class="col-md-8">{{ $user->pref }}{{ $user->address }}</dd>
+                            <dt class="col-md-4">電話番号</dt>
+                            <dd class="col-md-8">{{ $user->tel }}</dd>
+                            <dt class="col-md-4">誕生日</dt>
+                            <dd class="col-md-8">{{ $user->birthday }}</dd>
+                            <dt class="col-md-4">性別</dt>
+                            @if ($user->gender=='male')
+                            <dd class="col-md-8">男</dd>
+                            @else
+                            <dd class="col-md-8">女</dd>
+                            @endif
+                            <dt class="col-md-4">教室参加回数</dt>
+                            <dd class="col-md-8">{{ $class_reservation_times }}回</dd>
+                            <dt class="col-md-4">オンライン教室参加回数</dt>
+                            <dd class="col-md-8">{{ $zoom_reservation_times }}回</dd>
+                        </dl>
+                    </div>
+                    <a class="btn btn-link pull-right" href="javascript:history.back();"><i class="fas fa-backward"></i> 戻る</a>
                 </div>
-                <a class="btn btn-link pull-right" href="javascript:history.back();"><i class="fas fa-backward"></i> 戻る</a>
             </div>
         </div>
-    </div>
     </section>
 </div>
 @endsection
-
